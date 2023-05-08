aa0f893bb189f295bdd2a2db2fc1dabf955caf77
OViki <v.ozsvart@gmail.com>
2023-05-08 11:04:54 +0200

    First Component

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <p>Welcome to React!</p>
+            </div>
+        );
+    }
+}
+export default Hello;
