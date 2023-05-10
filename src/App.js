09a7b81791f2456320a7305ac5c0118fdc9916ae
OViki <v.ozsvart@gmail.com>
2023-05-10 21:52:12 +0200

    Styling the page and adding scroll component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from'./robots';
+import Scroll from './Scroll';
+import './App.css';
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield:'',
         }
     }
+
+componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> {
+            return response.json();
+        })
+        .then(users => {
+            this.setState({ robots: users })
+        })
+}
 
     onSearchChange = (event) => {
             this.setState({ searchfield: event.target.value })
@@ -21,13 +33,19 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (this.state.robots.length ===0) {
+            return <h1>Loading</h1>
+        } else {
         return (
             <div className='tc'>
-                <h1>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
                 <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
-        );   
+            );
+        }  
     }
 }
 
